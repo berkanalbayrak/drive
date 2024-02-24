ece3b96e96819496e24962c8f6d60c5ac127ec35
Berkan <berkanalbayrak1@gmail.com>
2024-02-24 13:57:05 +0300

    conditional free look

---
+++ Assets/Scripts/ProjectDrive/Camera/CineMachineConditionalFreeLook.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Cinemachine;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace ProjectDrive.Camera
+{
+    [RequireComponent(typeof(CinemachineFreeLook))]
+    public class CineMachineConditionalFreeLook : MonoBehaviour
+    {
+        private CinemachineFreeLook _freeLookCamera;
+        private float _xInputAxisDefaultSpeed;
+        private float _yInputAxisDefaultSpeed;
+
+        private void Awake()
+        {
+            _freeLookCamera = GetComponent<CinemachineFreeLook>();
+        }
+
+        private void Start()
+        {
+            _xInputAxisDefaultSpeed = _freeLookCamera.m_XAxis.m_MaxSpeed;
+            _yInputAxisDefaultSpeed = _freeLookCamera.m_YAxis.m_MaxSpeed;
+
+            Debug.Log(_xInputAxisDefaultSpeed);
+            Debug.Log(_yInputAxisDefaultSpeed);
+        }
+
+        private void OnDisable()
+        {
+            _freeLookCamera.m_XAxis.m_MaxSpeed = _xInputAxisDefaultSpeed;
+            _freeLookCamera.m_YAxis.m_MaxSpeed = _yInputAxisDefaultSpeed;
+        }
+
+        private void Update()
+        {
+            
+            var enableFreeLook = (Input.GetMouseButton(0) && !EventSystem.current.IsPointerOverGameObject());
+            
+            _freeLookCamera.m_XAxis.m_MaxSpeed = enableFreeLook ? _xInputAxisDefaultSpeed : 0;
+            _freeLookCamera.m_YAxis.m_MaxSpeed = enableFreeLook ? _yInputAxisDefaultSpeed : 0;
+        }
+    }
+}
